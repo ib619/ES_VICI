b171329a7675e3acf5caebf2f105a5d2c2cf4667
igorbodnar16 <igor.bodnar.prog@gmail.com>
2022-02-25 11:26:21 +0000

    update App

--- App/main.py
+++ App/main.py
@@ -1,12 +1,14 @@
 from kivy.app import App
 import paho.mqtt.client as mqtt
-from kivy.properties import StringProperty, BooleanProperty, NumericProperty
+from kivy.properties import StringProperty, BooleanProperty, NumericProperty, Clock
+from kivy.uix.screenmanager import ScreenManager
 
 
-class SPIKE(App):
+class MyScreenManager(ScreenManager):
     protect_status = StringProperty("Your drink is not protected")
     arm_button_disabled = BooleanProperty(False)
     arm_button_text = StringProperty("Arm")
+    arm_button_state = StringProperty("normal")
 
     connect_status = StringProperty("Please connect to your device")
     connect_button_text = StringProperty("Connect")
@@ -17,6 +19,26 @@ class SPIKE(App):
 
     disconnected_icon_width = NumericProperty(1)
     connected_icon_width = NumericProperty(0)
+    arm_bool = True
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def timer_callback(self, dt):
+        self.arm_bool = False
+        self.ids.arm_toggle.state = "normal"
+        self.arm_button_disabled = False
+        self.protect_status = "Could not connect: Please check connection status"
+        Clock.schedule_once(self.enable_arm, 1)
+
+    def enable_arm(self, dt):
+        self.arm_bool = True
+
+
+class SPIKE(App):
+    def build(self):
+        self.manager = MyScreenManager()
+        return self.manager
 
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -29,58 +51,61 @@ class SPIKE(App):
             self.mqttc.subscribe("VICI/test/disarmed", 0)
             self.mqttc.subscribe("VICI/test/spike", 0)
 
-
         def onMessage(client, userdata, msg):
             msg.payload = msg.payload.decode("utf-8")
             if msg.topic == "VICI/test/armed":
-                self.protect_status = "You drink is protected!"
-                self.arm_button_disabled = False
-                self.warning_width = 0
-                self.shield_protected_width = 1
-                self.shield_unprotected_width = 0
+                Clock.unschedule(self.arm_event)
+                self.manager.protect_status = "You drink is protected!"
+                self.manager.arm_button_disabled = False
+                self.manager.warning_width = 0
+                self.manager.shield_protected_width = 1
+                self.manager.shield_unprotected_width = 0
             if msg.topic == "VICI/test/disarmed":
-                self.protect_status = "You drink is not protected!"
-                self.arm_button_disabled = False
-                self.warning_width = 0
-                self.shield_protected_width = 0
-                self.shield_unprotected_width = 1
+                self.manager.protect_status = "You drink is not protected!"
+                self.manager.arm_button_disabled = False
+                self.manager.warning_width = 0
+                self.manager.shield_protected_width = 0
+                self.manager.shield_unprotected_width = 1
             if msg.topic == "VICI/test/spike":
-                self.protect_status = "DANGER:\n You drink was SPIKED!!!"
-                self.arm_button_disabled = False
-                self.warning_width = 1
-                self.shield_protected_width = 0
-                self.shield_unprotected_width = 0
+                self.manager.protect_status = "DANGER:\n You drink was SPIKED!!!"
+                self.manager.arm_button_disabled = False
+                self.manager.warning_width = 1
+                self.manager.shield_protected_width = 0
+                self.manager.shield_unprotected_width = 0
 
         self.mqttc.on_connect = onConnect
         self.mqttc.on_message = onMessage
-
-    def send_arm_message(self, widget):
-        if widget.state == "normal":
-            self.mqttc.publish("VICI/test/arm", "arm")
-            self.protect_status = "Disarming..."
-            self.arm_button_text = "Arm"
-            self.arm_button_disabled = True
-        else:
-            self.mqttc.publish("VICI/test/disarm", "disarm")
-            self.protect_status = "Arming..."
-            self.arm_button_text = "Disarm"
-            self.arm_button_disabled = True
 
     def connect_to_mqtt(self, widget):
         if widget.state == "down":
             self.mqttc.connect("test.mosquitto.org", 1883)
             self.mqttc.loop_start()
-            self.connect_status = "Connected"
-            self.connect_button_text = "Disconnect"
-            self.connected_icon_width = 1
-            self.disconnected_icon_width = 0
+            self.manager.connect_status = "Connected"
+            self.manager.connect_button_text = "Disconnect"
+            self.manager.connected_icon_width = 1
+            self.manager.disconnected_icon_width = 0
         else:
             self.mqttc.disconnect()
             self.mqttc.loop_stop()
-            self.connect_status = "Please connect to your device"
-            self.connect_button_text = "Connect"
-            self.connected_icon_width = 0
-            self.disconnected_icon_width = 1
+            self.manager.connect_status = "Please connect to your device"
+            self.manager.connect_button_text = "Connect"
+            self.manager.connected_icon_width = 0
+            self.manager.disconnected_icon_width = 1
+
+    def send_arm_message(self, widget):
+        if self.manager.arm_bool:
+            if widget.state == "normal":
+                self.mqttc.publish("VICI/test/disarm", "disarm")
+                self.manager.protect_status = "Disarming..."
+                self.manager.arm_button_text = "Arm"
+                self.manager.arm_button_disabled = True
+
+            else:
+                self.mqttc.publish("VICI/test/arm", "arm")
+                self.manager.protect_status = "Arming..."
+                self.manager.arm_button_text = "Disarm"
+                self.manager.arm_button_disabled = True
+                self.arm_event = Clock.schedule_once(self.manager.timer_callback, 8)
 
 
 SPIKE().run()
